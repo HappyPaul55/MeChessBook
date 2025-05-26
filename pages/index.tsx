dc6209b08b1137daf0d5ab2662e2d924f69a4428
Paul Hutchinson <paul@lexiphanic.co.uk>
2025-05-26 10:29:44 +0000

    Branding

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,7 @@ export default function HomePage() {
         </div>
 
         <Head>
+          <title>Me Chess Book</title>
           <style>
             {`
               @keyframes gradient {
